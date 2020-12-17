fa10f074ffc26d0fcdec6d047c8fa9125d6dcaa6
Javier Lopez <javierl.1994@gmail.com>
2020-12-17 10:51:04 -0600

    Adding about dialog.

--- lib/screens/movies_screen.dart
+++ lib/screens/movies_screen.dart
@@ -14,8 +14,9 @@ class MoviesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text(
-          'Movies',
+          'Movie App',
         ),
         actions: [
           IconButton(
@@ -27,6 +28,69 @@ class MoviesScreen extends StatelessWidget {
                 final moviesData = Provider.of<Movies>(context, listen: false);
                 moviesData.clearSearchResults();
                 Navigator.of(context).pushNamed(MovieSearchScreen.routeName);
+              }),
+          IconButton(
+              icon: Icon(Icons.help),
+              onPressed: () {
+                showAboutDialog(
+                    context: context,
+                    applicationName: 'Movie App',
+                    applicationVersion: '1.0.2',
+                    applicationLegalese: 'Develop by Javier Lopez',
+                    applicationIcon: Image.asset(
+                      'assets/images/movieicon.png',
+                      height: 60.0,
+                    ),
+                    children: [
+                      Text(
+                        'This app is meant to display what Flutter can do in a simple movie app. This app is using as a data source of movie the YTS API, you can go here to learn more https://yts.mx/api.',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'provider: ^4.3.2+2',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'intl: ^0.16.1',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'http: ^0.12.2',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'transparent_image: ^1.0.0',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'photo_view: ^0.10.2',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'youtube_player_flutter: ^7.0.0+7',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'cached_network_image: ^2.4.1',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ]);
               })
         ],
       ),
